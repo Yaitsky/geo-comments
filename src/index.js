d4e323f5103469aaa2133bde53e273dc4643a5ea
Igor Yaitsky <yaickiu@gmail.com>
2017-03-18 13:10:47 +0300

    add markers, click on markers, render comments

--- src/index.js
+++ src/index.js
@@ -1,108 +1,5 @@
-// require('./index.css');
-//
-// window.initMap = function() {
-//     // MAP STYLES
-//     var styles = [
-//         {
-//             stylers: [
-//                 { hue: "#ff8663" },
-//                 { saturation: -20 }
-//             ]
-//         },{
-//             featureType: "road",
-//             elementType: "geometry",
-//             stylers: [
-//                 { lightness: 100 },
-//                 { visibility: "simplified" }
-//             ]
-//         },{
-//             featureType: "road",
-//             elementType: "labels",
-//             stylers: [
-//                 { visibility: "off" }
-//             ]
-//         }
-//     ];
-//     var styledMap = new google.maps.StyledMapType(styles,
-//         {name: "Styled Map"});
-//
-//     var mapOptions = {
-//         zoom: 16,
-//         center: {lat: 55.752484, lng: 37.614513},
-//         disableDefaultUI: true,
-//         mapTypeControlOptions: {
-//             mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
-//         }
-//     }
-//     var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-//
-//     map.mapTypes.set('map_style', styledMap);
-//     map.setMapTypeId('map_style');
-//
-//     //VARIABLES
-//     var modal = document.querySelector('.modal'),
-//         locationTitleComments = document.querySelector('.modal__title'),
-//         userNameInput = document.querySelector('.userName'),
-//         locationTitleInput = document.querySelector('.locationName'),
-//         commentTextInput = document.querySelector('.add-comment__text'),
-//         userNameComments = document.querySelector('.comments__name'),
-//         locationAndDateInfo = document.querySelector('.comments__info');
-//
-//     // CREATE MARKER AND COMMENTS
-//     map.addListener('click', function (e) {
-//         var x = e.pixel.x;
-//         var y = e.pixel.y;
-//         var lat = e.latLng.lat();
-//         var lng = e.latLng.lng();
-//
-//         modal.style.display = 'block';
-//         modal.style.top = y + 'px';
-//         modal.style.left = x + 'px';
-//
-//         modal.addEventListener('click',function (e) {
-//             var closeButton = modal.querySelector('.close-button');
-//             if (e.target != closeButton) {
-//                 return;
-//             }
-//             modal.style.display = 'none';
-//         })
-//
-//         modal.addEventListener('click', function (e) {
-//             var addButton = modal.querySelector('.add-button');
-//             if (e.target != addButton) {
-//                 return;
-//             }
-//             var userName = userNameInput.value,
-//                 locationTitle = locationTitleInput.value,
-//                 commentText = commentTextInput.value;
-//
-//             var marker2 = new google.maps.Marker({
-//                 position: {lat: lat, lng: lng},
-//                 map: map,
-//                 comments: []
-//             })
-//
-//             marker2.comments.push({
-//                 locationTitle: locationTitle,
-//                 userName: userName,
-//                 commentText: commentText
-//             })
-//
-//             console.log(marker2.comments);
-//
-//             userNameInput.value = '';
-//             locationTitleInput.value = '';
-//             commentTextInput.value = '';
-//         })
-//
-//     })
-//
-//     function renderComments() {
-//
-//     }
-// }
-
 require('./index.css');
+
 var myMap;
 window.initMap = function() {
     // MAP STYLES
@@ -147,82 +44,163 @@ window.initMap = function() {
 }
 
 var mapWorker = {
+    markersArray: [],
+    markersCounter: 0,
+    lats: [],
+    lngs: [],
+    clickOnMarkerFlag: false,
+    currentIndex: 0,
     initListeners: function() {
-        console.log(myMap, google)
         //VARIABLES
         this.modal = document.querySelector('.modal');
-        this.locationTitleComments = document.querySelector('.modal__title');
+        this.closeButton = this.modal.querySelector('.close-button');
+        this.addButton = this.modal.querySelector('.add-button');
+
+        this.commentsList = document.querySelector('.comments__list');
         this.userNameInput = document.querySelector('.userName');
         this.locationTitleInput = document.querySelector('.locationName');
         this.commentTextInput = document.querySelector('.add-comment__text');
-        this.userNameComments = document.querySelector('.comments__name');
-        this.locationAndDateInfo = document.querySelector('.comments__info');
-        this.markers = [];
-        // CREATE MARKER AND COMMENTS
+
         var self = this;
-        this.modal.addEventListener('click', this.checkClick.bind(this))
+        // CREATE MARKER AND COMMENTS
+        this.closeButton.addEventListener('click', this.closeModal.bind(this));
+        this.addButton.addEventListener('click', this.addMarkerAndComment.bind(this));
 
         myMap.addListener('click', function (e) {
-            this.isCreated = false;
-            console.log('catch!!!I am MAP', this.isCreated);
-            var x = e.pixel.x;
-            var y = e.pixel.y;
+            var x = window.event.pageX;
+            var y = window.event.pageY;
             self.lat = e.latLng.lat();
             self.lng = e.latLng.lng();
+            this.clickOnMarkerFlag = false;
 
+            //reverse  geocoding to get address
+            var latlng = {lat: self.lat, lng: self.lng};
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode({'location': latlng}, function (results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    this.locationAddress = results[0].formatted_address;
+                    this.locationTitleComments = document.querySelector('.modal__title-text');
+                    this.locationTitleComments.innerText = this.locationAddress;
+                }
+            })
+
+            // show modal window
+            self.commentsList.innerHTML = '';
             self.modal.style.display = 'block';
             self.modal.style.top = y + 'px';
             self.modal.style.left = x + 'px';
-        })
+        });
     },
-    checkClick: function (e) {
-        console.log(this.isCreated);
-        var closeButton = this.modal.querySelector('.close-button');
-        var addButton = this.modal.querySelector('.add-button');
+    closeModal: function () {
+        this.modal.style.display = 'none';
+    },
+    addMarkerAndComment: function () {
         var userName = this.userNameInput.value,
             locationTitle = this.locationTitleInput.value,
             commentText = this.commentTextInput.value;
-        if (e.target === closeButton) {
-            this.modal.style.display = 'none';
-        }
-        if (e.target === addButton && !this.isCreated) {
-            console.log(this.lat, this.lng);
-            var marker2 = new google.maps.Marker({
+
+        if (this.lats.indexOf(this.lat) < 0 && this.lngs.indexOf(this.lng) < 0) {
+
+            this.markersArray[this.markersCounter] = new google.maps.Marker({
                 position: {lat: this.lat, lng: this.lng},
                 map: myMap,
                 comments: []
-            })
+            });
 
-            marker2.comments.push({
+            this.markersArray[this.markersCounter].comments.push({
                 locationTitle: locationTitle,
                 userName: userName,
-                commentText: commentText
-            })
-            marker2.addListener('click', function (e) {console.log('catch!!!I am MARKER')})
+                commentText: commentText,
+                commentDate: this.getCommentDate()
+            });
+
             this.userNameInput.value = '';
             this.locationTitleInput.value = '';
             this.commentTextInput.value = '';
-            console.log('here ADD MARKER')
 
-            this.markers.push(marker2);
-            console.log(this.markers);
-            this.isCreated = true;
-        } else if (e.target === addButton) {
-            console.log('here ADD CLUSTER')
-            console.log(this.markers);
-            this.markers[this.markers.length - 1].comments.push({
+            this.commentsList.innerHTML = this.createComments(this.markersArray[this.markersCounter].comments);
+
+            this.markersArray[this.markersCounter].addListener('click', this.clickOnMarkerAndShowModal);
+
+            this.markersCounter++;
+
+            this.lats = [];
+            this.lngs = [];
+
+            for (var i = 0; i < this.markersArray.length; i++) {
+                this.lats.push(this.markersArray[i].position.lat());
+                this.lngs.push(this.markersArray[i].position.lng());
+            }
+
+        } else {
+            var markerIndex;
+            if (this.clickOnMarkerFlag === true) {
+                markerIndex = this.currentIndex;
+            } else {
+                markerIndex = this.lats.indexOf(this.lat);
+            }
+
+            this.markersArray[markerIndex].comments.push({
                 locationTitle: locationTitle,
                 userName: userName,
-                commentText: commentText
-            })
+                commentText: commentText,
+                commentDate: this.getCommentDate()
+            });
+
+            this.commentsList.innerHTML = this.createComments(this.markersArray[markerIndex].comments);
+
             this.userNameInput.value = '';
             this.locationTitleInput.value = '';
             this.commentTextInput.value = '';
-            console.log(mapWorker.markers);
-            this.isCreated = false;
         }
+    },
+    createComments: function (comments) {
+        var templateFn = require('../comment-template.hbs');
+
+        return templateFn({
+            comments: comments
+        });
+    },
+    getCommentDate: function () {
+        var Data = new Date();
+        var year = Data.getFullYear();
+        var month = Data.getMonth();
+        var data = Data.getDate();
+        var fMonth;
+
+        switch (month)
+        {
+            case 0: fMonth="января"; break;
+            case 1: fMonth="февраля"; break;
+            case 2: fMonth="марта"; break;
+            case 3: fMonth="апреля"; break;
+            case 4: fMonth="мая"; break;
+            case 5: fMonth="июня"; break;
+            case 6: fMonth="июля"; break;
+            case 7: fMonth="августа"; break;
+            case 8: fMonth="сентября"; break;
+            case 9: fMonth="октября"; break;
+            case 10: fMonth="ноября"; break;
+            case 11: fMonth="декабря"; break;
+        }
+
+        var currentDate = data + ' ' + fMonth + ' '+ year + ' г.';
+
+        return currentDate;
+    },
+    clickOnMarkerAndShowModal: function () {
+        var x = window.event.pageX;
+        var y = window.event.pageY;
+
+        mapWorker.currentIndex = mapWorker.lats.indexOf(this.position.lat());
+        mapWorker.clickOnMarkerFlag = true;
+
+        mapWorker.modal.style.display = 'block';
+        mapWorker.modal.style.top = y + 'px';
+        mapWorker.modal.style.left = x + 'px';
+
+        mapWorker.commentsList.innerHTML = mapWorker.createComments(this.comments);
     }
 }
-
 
 
